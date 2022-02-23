73546b2329c4c0ac7b1e7b2384aa085b873cecae
MadaraGS <madaragelsberga@inbox.lv>
2022-02-23 15:46:36 +0200

    FizzBuzz game

--- Third_homework/script.js
+++ Third_homework/script.js
@@ -1,41 +1,65 @@
-for(let i=1; i<=100; i++){
-    
-    if((i % 5 === 0) && (i % 3 === 0)){
-        console.log("FizzBuzz")
-    }
-    else if(i % 3 === 0){
-        console.log("Fizz")
-    }
-    else if(i % 5 === 0){
-        console.log("Buzz")
-    } else{
-        console.log(i);
-    }
-
-}
-
-for(let i=1; i<=100; i++){
-    
-    if(i % 15 === 0){
-        console.log("FizzBuzz")
-    }
-    else if(i % 3 === 0){
-        console.log("Fizz")
-    }
-    else if(i % 5 === 0){
-        console.log("Buzz")
-    } else{
-        console.log(i);
-    }
-
-}
+// for(let i=1; i<=100; i++){
+//     if((i % 5 === 0) && (i % 3 === 0)){
+//         console.log(i)
+//     }
+// //     else if(i % 3 === 0){
+// //         console.log("Fizz")
+// //     }
+// //     else if(i % 5 === 0){
+// //         console.log("Buzz")
+// //     } else{
+// //        // console.log(i);
+// //     }
+// // }
 
 
-let num1 = 3;
-let num2 = 5;
-let num3 = 15;
+// let number = prompt("Enter number how many FizzBuzz you want to see")
+//  for(let i = 1;  i <= number; i++){
+//         let summ = i * 15;
+//      if( summ % 15  === 0){
+//          console.log(summ);
+            
+//      }
+//  }
 
-for(let i=1; i<=100; i++){
+ 
 
-    i % num3 === 0 ? console.log( "FizzBuzz") : i % num1 === 0 ? console.log ("Fizz") :  i % num2 === 0 ? console.log("Buzz") : console.log(i)
-}
+
+//     if((i % 5 === 0) && (i % 3 === 0)){
+//         console.log("FizzBuzz")
+//     }
+//     else if(i % 3 === 0){
+//         console.log("Fizz")
+//     }
+//     else if(i % 5 === 0){
+//         console.log("Buzz")
+//     } else{
+//         console.log(i);
+//     }
+
+// }
+
+// for(let i=1; i<=100; i++){
+//     if(i % 15 === 0){
+//         console.log(i)
+//     }
+    // else if(i % 3 === 0){
+    //     console.log("Fizz")
+    // }
+    // else if(i % 5 === 0){
+    //     console.log("Buzz")
+    // } else{
+    //     console.log(i);
+    // }
+
+// }
+
+
+// let num1 = 3;
+// let num2 = 5;
+// let num3 = 15;
+
+// for(let i=1; i<=100; i++){
+
+//     i % num3 === 0 ? console.log( "FizzBuzz") : i % num1 === 0 ? console.log ("Fizz") :  i % num2 === 0 ? console.log("Buzz") : console.log(i)
+// }
